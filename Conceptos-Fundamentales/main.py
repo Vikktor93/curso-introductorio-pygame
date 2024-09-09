bca8af4fada860bf5498745c601617a8a3b9dfa0
Vikktor93 <Vikktor93@users.noreply.github.com>
2024-09-09 18:04:29 -0300

    Se agrega comentarios al código

--- Conceptos-Fundamentales/main.py
+++ Conceptos-Fundamentales/main.py
@@ -1,11 +1,11 @@
 import pygame
 
-# Inicialización de Pygame
+# Se inicializa Pygame
 pygame.init()
 
 # Configuración de la ventana
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Ejemplo con pygame.draw")
+ventana = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Figuras Geométricas")
 
 # Color de fondo
 background_color = (0, 0, 0)
@@ -17,19 +17,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Pintar el fondo
-    screen.fill(background_color)
+    # Se pinta el fondo con el color negro definido previamente
+    ventana.fill(background_color)
 
     # Formas geométricas
-    pygame.draw.circle(screen, (255, 0, 0), (400, 300), 50)  # Círculo rojo
-    pygame.draw.rect(screen, (0, 255, 0), (150, 200, 100, 50))  # Rectángulo verde
-    pygame.draw.line(screen, (0, 0, 255), (100, 100), (700, 100), 5)  # Línea azul
+    pygame.draw.circle(ventana, (255, 0, 0), (400, 300), 50)  # Círculo rojo
+    pygame.draw.rect(ventana, (0, 255, 0), (150, 200, 100, 50))  # Rectángulo verde
+    pygame.draw.line(ventana, (0, 0, 255), (100, 100), (700, 100), 5)  # Línea azul
     
     # Triángulo
-    triangle_points = [(400, 100), (350, 200), (450, 200)]  # Tres puntos del triángulo
-    pygame.draw.polygon(screen, (255, 255, 0), triangle_points)  # Triángulo amarillo
+    vertices_triangulo = [(400, 110), (350, 200), (450, 200)]  # Tres puntos del triángulo
+    pygame.draw.polygon(ventana, (255, 255, 0), vertices_triangulo)  # Triángulo amarillo
 
-    # Actualizar la pantalla
+    # Se actualiza la pantalla
     pygame.display.update()
 
 # Cerrar Pygame
